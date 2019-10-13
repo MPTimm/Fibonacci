7bee2a5d14cffab687f7892e7d7c5832f6a0ee5f
MPTimm <matheusptimm@gmail.com>
2019-10-13 15:43:31 -0300

    Fibonacci's Theorem

---
+++ Fibonacci's Theorem/Fibonacci's Theorem/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci_s_Theorem
+{
+    class Fibonacci
+    {
+        private int[] Numeros = new int[] { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        public void theorem()
+        {
+            for(int i = 0; i <= Numeros.Length - 3; i++)
+            {
+                
+                    Numeros[i + 2] = Numeros[i] + Numeros[i + 1];
+                
+
+                
+            }
+
+            Array.ForEach(Numeros, Console.WriteLine);
+        }
+    }
+}
---
+++ Fibonacci's Theorem/Fibonacci's Theorem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fibonacci_s_Theorem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibonacci = new Fibonacci();
+
+            fibonacci.theorem();
+        }
+    }
+}
